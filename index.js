e1c85c513ea8e5d2fa80ddd55f267f8b0e5281b3
Tyler Buchea <tyler@buchea.com>
2021-12-03 10:50:42 -0800

    Move settings to top

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const fs = require("fs").promises;
 
 const MAX_CHOICES = 30;
 const SEED_PHRASE = "cashgrab";
+const MIN = 1;
+const MAX = 140;
 
 const engine = Provable({ count: 10000, seed: SEED_PHRASE });
 const choices = [];
 
 function makeChoice() {
   const nextHash = engine.next();
-  const nextChoice = Provable.toFloat(nextHash, 1, 140, true);
+  const nextChoice = Provable.toFloat(nextHash, MIN, MAX, true);
   const nextChoiceInt = Math.floor(nextChoice);
   const isUnique = !choices.includes(nextChoiceInt);
   if (isUnique) choices.push(nextChoiceInt);
