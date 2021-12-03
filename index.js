9b1fdba4d3a3db17cd90af6b741655fe698b0724
Tyler Buchea <tyler@buchea.com>
2021-12-03 10:49:02 -0800

    clean up code

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const Provable = require("provable");
 const fs = require("fs").promises;
 
-// count default:1, number of hashes in the series to produce, takes longer depending on how big the number is
-// seed defaults to some random string with letters and numbers
-const engine = Provable({ count: 10000, seed: "cashgrab" });
+const SEED_PHRASE = "cashgrab";
+const engine = Provable({ count: 10000, seed: SEED_PHRASE });
 
-// return a random int32 and increments the internal state to the next hash
-// const int32 = engine();
-
-// internal state of the engine. Use this to save and resume your engine.
 const choices = [];
 const maxChoices = 30;
 
@@ -26,15 +21,4 @@ function makeChoice() {
 makeChoice();
 
 const fileNames = choices.map((choice) => `${choice}.png`);
-console.log(
-  "These numbers should be the same:",
-  fileNames.length,
-  new Set(fileNames).size
-);
-
 fs.writeFile("./choices.csv", fileNames.join("\r\n"));
-
-// resuming will re-generate the entire hash chain
-// from your seed and pick up where you left off with the index
-// const state = engine.state();
-// const resumedEngine = Provable(state);
